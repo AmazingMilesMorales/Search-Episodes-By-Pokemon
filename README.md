91b4aaa3ed352e373fe610045b1eea80bb6686b1
Leo Llopiz <lxl170000@utdallas.edu>
2022-06-19 21:44:47 -0500

    Made menu even more streamlined
    If everything works as it should be, the other options aren't necessary, so I hid them behind a help menu to lessen clutter

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Search-Episodes-By-Pokemon
 
-Python program that displays a list of episodes that a Pokemon has appeared in
+Python program that displays a list of episodes that a Pokemon has appeared in. Run main.py to start.
--- main.py
+++ main.py
@@ -3,27 +3,11 @@ import sqlite3
 # Local
 import sql
 
-def formatPokemonName(species):
-    appendableShortOptions = ['-udb', '-cdb', '-qdb']
-    appendableLongOptions = ['createdb', 'updatedb', 'quickdb', 'exit']
-    # TODO: Replace with regex
-    for option in appendableShortOptions:
-        species = species.replace(' ' + option, '')
-        species = species.replace(option, '')
-    for option in appendableLongOptions:
-        species = species.replace('-- ' + option, '')
-        species = species.replace('- ' + option, '')
-        species = species.replace(' ' + option, '')
-        species = species.replace(option, '')
-    
+def formatPokemonName(species): 
     species = species[:1].upper() + species[1:] #TODO Fix capitalization, this is not ideal
     return species
 
-print("You can append with the following options:")
-print("* -udb or updatedb to update episodes database with new episodes from and saved to the pokemonMediaInfo.p file (Recommended for first time or if it's been a while since database has been updated)")
-print("* -cdb or createdb to get a fully updated database from scratch using Bulbapedia API and save to pokemonMediaInfo.p (Will take a few minutes and might cause an error, useful if pokemonMediaInfo.p is missing/corrupted)")
-print("* -qdb or quickdb to create episodes database from the pokemonMediaInfo.p file (Useful if pokemonanime.db is missing/corrupted)")
-print("* exit to exit program")
+print("If this is your first time running this program or if it has been a while, type 'udb' to ensure an updated database. Type 'help' for more commands")
 print("Otherwise, what Pokemon would you like to look up?")
 
 choice = input()
@@ -33,20 +17,26 @@ while(True):
         print("What Pokemon would you like to look up?")
         choice = input()  
 
-    if "createdb" in choice or "-cdb" in choice:
-        print("Creating database from scratch...")
-        sql.createDatabase()
-    elif "quickdb" in choice or "-qdb" in choice:
-        print("Creating database quickly...")
-        sql.createDatabaseFromSavedFile()
-    elif"updatedb" in choice or "-udb" in choice:
-        print("Updating database file...")
-        sql.updateDatabaseFromSavedFile()
-    elif "exit" in choice:
+    if choice == "help":
+        print("* exit to exit program")
+        print("* help for list of commands")
+        print("* udb or updatedb to update episodes database with new episodes from and saved to the pokemonMediaInfo.p file (Recommended for first time or if it's been a while since database has been updated)")
+        print("* cdb or createdb to get a fully updated database from scratch using Bulbapedia API and save to pokemonMediaInfo.p (Will take a few minutes and might cause an error, useful if pokemonMediaInfo.p is missing/corrupted)")
+        print("* qdb or quickdb to create episodes database from the pokemonMediaInfo.p file (Useful if pokemonanime.db is missing/corrupted)")
+    elif choice == "exit":
         print("Exiting program...")
         exit()
-
-    # Regardless, search for that Pokemon!!    
-    choice = formatPokemonName(choice)
-    sql.getMediaByPokemonName(choice)
+    elif choice == "createdb" or choice == "cdb":
+        print("Creating database from scratch...")
+        sql.createDatabase()
+    elif choice == "quickdb" or choice == "qdb":
+        print("Creating database quickly...")
+        sql.createDatabaseFromSavedFile()
+    elif choice == "updatedb" or choice == "udb":
+        print("Updating database file...")
+        sql.updateDatabaseFromSavedFile()
+    else:
+        choice = formatPokemonName(choice)
+        if choice: sql.getMediaByPokemonName(choice)
+        else: print("Error parsing input, maybe you mispelled?")
     firstTimeSearching = False
